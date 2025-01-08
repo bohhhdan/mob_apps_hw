2a842f469f32d26d766b2188c0d34eed2b0e67b7
Muhammad Ali <94305336+aleee-chaudhary@users.noreply.github.com>
2025-01-08 13:58:03 +0100

    Fragment Login and Register

--- app/src/main/java/com/example/myapplicationlast/FragmentsActivity.kt
+++ app/src/main/java/com/example/myapplicationlast/FragmentsActivity.kt
@@ -8,7 +8,7 @@ import androidx.fragment.app.Fragment
 class FragmentsActivity : AppCompatActivity() {
 
     private lateinit var switchFragmentButton: Button
-    private val isFragmentA = true
+    private val isFragmentLogin = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragments)
@@ -16,23 +16,26 @@ class FragmentsActivity : AppCompatActivity() {
         // Start with the LoginFragment when the activity is first created
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainer, FragmentA())
+                .replace(R.id.fragmentContainer, LoginFragment())
                 .commit()
         }
 
         // Switch between fragments when the button is clicked
         switchFragmentButton = findViewById(R.id.switchFragmentButton)
         switchFragmentButton.setOnClickListener {
-            val fragment_manager=supportFragmentManager
-            val transaction=fragment_manager.beginTransaction()
+            val currentFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
 
-            if (isFragmentA) {
-                transaction.replace(R.id.fragmentContainer, FragmentB())
+            if (currentFragment is LoginFragment) {
+                showFragment(RegisterFragment())
             } else {
-                transaction.replace(R.id.fragmentContainer, FragmentA())
+                showFragment(LoginFragment())
             }
-
-            transaction.commit()
         }
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }
